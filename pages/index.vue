e99e8ea0e59a71a296b36073f34332c18caf8262
celvineadiputra_ <celvineadiputra2@gmail.com>
2020-12-08 21:06:48 +0700

    init project

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-app>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <button class="btn btn-danger">Hello My Button</button>
+        </div>
+      </div>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
