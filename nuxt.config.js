cc4fd45b1251786669ece5891cffd117bf0cb350
celvineadiputra-dev <celvineadiputra2@gmail.com>
2020-12-11 10:40:46 +0700

    set PWA

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,39 +1,61 @@
 export default {
-    // Global page headers (https://go.nuxtjs.dev/config-head)
-    head: {
-        title: 'onex',
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-        ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-        script: [{ src: '/js/bootstrap.min.js' },{src:'https://scripts.sirv.com/sirv.js'}],
-    },
-
-    // Global CSS (https://go.nuxtjs.dev/config-css)
-    css: ['~/assets/css/style.css'],
-
-    // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-    plugins: [],
-
-    // Auto import components (https://go.nuxtjs.dev/config-components)
-    components: true,
-
-    // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-    buildModules: [],
-
-    // Modules (https://go.nuxtjs.dev/config-modules)
-    modules: [
-        // https://go.nuxtjs.dev/axios
-        '@nuxtjs/axios',
-        // https://go.nuxtjs.dev/pwa
-        '@nuxtjs/pwa',
+  // Global page headers (https://go.nuxtjs.dev/config-head)
+  head: {
+    title: 'onex',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
     ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      { src: '/js/bootstrap.min.js' },
+      { src: 'https://scripts.sirv.com/sirv.js' },
+    ],
+  },
 
-    // Axios module configuration (https://go.nuxtjs.dev/config-axios)
-    axios: {},
+  // Global CSS (https://go.nuxtjs.dev/config-css)
+  css: ['~/assets/css/style.css'],
 
-    // Build Configuration (https://go.nuxtjs.dev/config-build)
-    build: {},
+  // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
+  plugins: [],
+
+  // Auto import components (https://go.nuxtjs.dev/config-components)
+  components: true,
+
+  // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
+  buildModules: ['@nuxtjs/pwa'],
+
+  // Modules (https://go.nuxtjs.dev/config-modules)
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    // https://go.nuxtjs.dev/pwa
+    '@nuxtjs/pwa',
+  ],
+
+  // Axios module configuration (https://go.nuxtjs.dev/config-axios)
+  axios: {},
+
+  // Build Configuration (https://go.nuxtjs.dev/config-build)
+  build: {},
+
+  pwa: {
+    manifest: {
+      name: 'onexFoods',
+      lang: 'en',
+      useWebmanifestExtension: false,
+    },
+  },
+
+  workbox: {
+    runtimeCaching: [
+      {
+        urlPattern: 'https://scripts.sirv.com/sirv.js',
+      },
+      {
+        urlPattern: 'https://ouscerea.sirv.com/onexFoods/.*',
+      },
+    ],
+  },
 }
