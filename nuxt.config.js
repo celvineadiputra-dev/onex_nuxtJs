3a727517cdcdbf2e2b3e380add4fd2c18bc40f51
celvineadiputra_ <celvineadiputra2@gmail.com>
2020-12-12 12:13:32 +0700

    add love

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,10 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: '/css/all.css' },
+    ],
     script: [
       { src: '/js/bootstrap.min.js' },
       { src: 'https://scripts.sirv.com/sirv.js' },
@@ -15,7 +18,7 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['~/assets/css/style.css'],
+  css: ['@/assets/css/style.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [],
