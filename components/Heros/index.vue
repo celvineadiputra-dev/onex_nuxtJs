e0f4e11e0ff1f8cd12164a0581cfa1f41fb59e7b
celvineadiputra-dev <celvineadiputra2@gmail.com>
2020-12-13 19:25:16 +0700

    update hero image

--- components/Heros/index.vue
+++ components/Heros/index.vue
@@ -21,7 +21,7 @@
           />
         </div>
         <div class="col-sm-12 col-md-6 order-sm-0 order-md-1 order-0">
-          <img class="Sirv img-fluid" data-src="https://ouscerea.sirv.com/onexFoods/video.png?format=webp&q=50" alt="imageHeroChef" />
+          <img class="Sirv img-fluid" data-src="https://ouscerea.sirv.com/onexFoods/Hero.png?format=webp&q=60" alt="imageHeroChef" />
         </div>
     </div>
   </div>
