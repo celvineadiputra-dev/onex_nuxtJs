aa315f095b30eed6a0c35bfb2088cc47be9442c6
celvineadiputra-dev <celvineadiputra2@gmail.com>
2020-12-21 09:19:16 +0700

    test api

--- components/Heros/index.vue
+++ components/Heros/index.vue
@@ -2,13 +2,15 @@
   <div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-6 d-flex justify-content-center flex-column order-sm-1 order-md-0 order-1">
-          <h1 class="headline col-sm-12 col-md-10" data-aos="fade-right">
+          <h1 class="headline col-sm-12 col-md-10" data-aos="fade-right" data-aos-duration="2000">
             Eat What You Cook
             With Us, Together!
           </h1>
           <p class="detailHeadline col-sm-12 col-md-8">
             When you eat something that cooked by
             yourself, the happiness is priceless.
+            <br>
+            {{code}}
           </p>
           <bread-button
             text="Decide a Menu"
@@ -32,6 +34,17 @@ import breadButton from '../Buttons/breadButton.vue'
 export default {
   components: { breadButton },
   name: 'Hero',
+  data : function(){
+    return{
+      code : []
+    }
+  },
+  async fetch(){
+    this.code = await fetch('https://jsonplaceholder.typicode.com/todos/1').then(res=>res.json()).then(res=>{
+      console.log(res);
+      return res;
+    });
+  }
 }
 </script>
 
